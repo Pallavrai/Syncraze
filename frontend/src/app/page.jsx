fe771a11246ba961a62c9f0e4f8b37aae6dbdfe7
MohammadAshif1001 <mohammadashif1000@gmail.com>
2023-11-02 21:55:10 +0530

    fix

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import ParticleBackground from "../particale-bg";
 
 export default function Home() {
   return  (
--- frontend/src/particale-bg.js
+++
@@ -1,9 +0,0 @@
-import Particles from "react-particles-js"
-import particlesConfig from "./particle/particle-config";
-
-
-export default function ParticleBackground(){
-    return (        
-        <Particles prams={particlesConfig}></Particles>
-        );
-}
