610a037f5dbc0e145746802ad3ccad031b38aa68
MohammadAshif1001 <mohammadashif1000@gmail.com>
2023-11-02 21:53:29 +0530

    geting error fix it

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import ParticleBackground from "../particale-bg";
 
 export default function Home() {
-  return <div className="main">this is homepage</div>;
+  return  (
+    <div>
+      <ParticleBackground />
+      <CenterTitle />
+    </div>
+  );
+}
+
+function CenterTitle(){
+  return (
+      <div className="main bg-slate-950 h-screen flex justify-evenly text-white">This is homepage</div>
+  );
 }
